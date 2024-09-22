ebe4fc27b55c4955c4341f17ae4e8e1a202c97d9
Ai-ZL <128574325+Ai-ZL@users.noreply.github.com>
2024-09-22 19:10:47 +0800

    Update optimize_model.py

--- optimize_model.py
+++ optimize_model.py
@@ -1,4 +1,4 @@
 from onnxruntime.quantization.shape_inference import quant_pre_process
-quant_pre_process(input_model='model.onnx',
-                  output_model_path='model_optimize.onnx',
+quant_pre_process(input_model='model.onnx', #model before processing
+                  output_model_path='model_opt.onnx', #stored mmodel after processing
                   auto_merge=True)
