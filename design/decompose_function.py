fb4711176e68c448f18f91af5271642206b745e9
Ai-ZL <128574325+Ai-ZL@users.noreply.github.com>
2024-10-15 14:59:05 +0800

    Update decompose_function.py

--- design/decompose_function.py
+++ design/decompose_function.py
@@ -3,21 +3,22 @@ import numpy as np
 import argparse
 
 
-
+#Arguments for running py files
 parser = argparse.ArgumentParser()
 parser.add_argument('-model', '--model_path', help='model stored path')
 parser.add_argument('-model_o', '--model_path_out', help='output model stored path')
 parser.add_argument('-m', '--mode', help='three decomposition mode: sigmoid, softmax, exp')
 args = parser.parse_args()
 
-onnx_model = onnx.load(args.model_path)
-graph = onnx_model.graph
-node = graph.node
+onnx_model = onnx.load(args.model_path) #model need to modify
+graph = onnx_model.graph # onnx model graph
+node = graph.node # onnx model node
 
 
-all_initializer = onnx_model.graph.initializer
+all_initializer = onnx_model.graph.initializer # onnx model initializer
 
 
+#check model whether is valid
 try:
     onnx.checker.check_model(onnx_model)
 except onnx.checker.ValidationError as e:
@@ -25,21 +26,22 @@ except onnx.checker.ValidationError as e:
 else:
     print('The model is valid!')
 
+#check model opset version, all operator version should equal or older than this
 print("original version ", onnx_model.opset_import)
 
 
 ############decompose all sigmoid function#################
 if args.mode == 'sigmoid':
     for i,j in enumerate(node):
-        if j.name[-7:] == 'Sigmoid':
-            print("######%s######" % i)
+        if j.name[-7:] == 'Sigmoid': # The sigmoid node name should end with “Sigmoid”.
+            print("######%s######" % i) # node index
             old_node = j
-            onnx_model.graph.node.remove(j)
+            onnx_model.graph.node.remove(j) # remove the old sigmoid node
             print('======update ',old_node.name,'=========')
             print("==========add initializer===========")
             initia_name = old_node.name+'_add_b'
             initia_name2 = old_node.name + '_divide_a'
-            initializer_new = onnx.helper.make_tensor(
+            initializer_new = onnx.helper.make_tensor( #creat initializer
 
                 name=initia_name,
 
@@ -55,14 +57,14 @@ if args.mode == 'sigmoid':
                 vals=[1],
                 dims=[]
             )
-            graph.initializer.insert(0, initializer_new)
+            graph.initializer.insert(0, initializer_new) # add new initializer
             graph.initializer.insert(0, initializer_new2)
-            # 添加各种节点分解sigmoid
+            # 添加各种节点分解sigmoid (according to sigmoid function: y = 1 / (1 + exp(-x)))
             neg_output = old_node.output[0]+'_neg'
             neg_name = old_node.name+'_Neg'
             print("neg name is ",neg_name)
             print("neg output is ",neg_output)
-            new_scale_node_1 = onnx.helper.make_node(
+            new_scale_node_1 = onnx.helper.make_node( # creat node
                 op_type="Neg",
                 inputs=old_node.input,
                 outputs=[neg_output],
@@ -92,7 +94,7 @@ if args.mode == 'sigmoid':
                 name=div_name,
             )
 
-            graph.node.insert(i, new_scale_node_1)
+            graph.node.insert(i, new_scale_node_1) # add new node
             graph.node.insert(i + 1, new_scale_node_2)
             graph.node.insert(i + 2, new_scale_node_3)
             graph.node.insert(i + 3, new_scale_node_4)
@@ -104,13 +106,14 @@ if args.mode == 'sigmoid':
 if args.mode == 'softmax':
     select_axes = np.array([-1], dtype=np.int64)
     for i,j in enumerate(node):
-        if j.name == "/LogSoftmax":
+        if j.name == "/LogSoftmax": # decompose logsoftmax node, log(softmax(x))
             print("######%s######" % i)
             old_node = j
             print('======update ', old_node.name, '=========')
-            onnx_model.graph.node.remove(j)
-            axes1 = onnx.numpy_helper.from_array(select_axes, "logprobs_reducesum_axes")
-            new_scale_node_2 = onnx.helper.make_node(
+            onnx_model.graph.node.remove(j) # remove logsoftmax node
+            axes1 = onnx.numpy_helper.from_array(select_axes, "logprobs_reducesum_axes") #creat initializer
+            # avoid overflows, subtract maximal value
+            new_scale_node_2 = onnx.helper.make_node( # creat new node
                 op_type="ReduceMax",
                 inputs=old_node.input,
                 outputs=['logprobs_reducemax'],
@@ -148,8 +151,8 @@ if args.mode == 'softmax':
                 outputs=old_node.output,
                 name="/LogSoftmax_Log",
             )
-            graph.node.insert(i, new_scale_node_1)
-            all_initializer.insert(0, axes1)
+            graph.node.insert(i, new_scale_node_1) #insert new node
+            all_initializer.insert(0, axes1) #insert new initializer
             graph.node.insert(i + 1, new_scale_node_2)
             graph.node.insert(i + 2, new_scale_node_3)
             graph.node.insert(i + 3, new_scale_node_4)
@@ -157,17 +160,18 @@ if args.mode == 'softmax':
             graph.node.insert(i + 5, new_scale_node_6)
 
     select_axes2 = np.array([-1], dtype=np.int64)
-    for i, j in enumerate(node):
-        if j.name[-8:] == "/Softmax":
+    for i, j in enumerate(node): # decompose other softmax node
+        if j.name[-8:] == "/Softmax":# The softmax node name should end with “/Softmax”.
             print("######%s######" % i)
             old_node = j
             print('======update ', old_node.name, '=========')
-            onnx_model.graph.node.remove(j)
+            onnx_model.graph.node.remove(j) #remove old sigmoid node
             initia_name = old_node.output[0]+"_reducesum_axes"
-            axes1 = onnx.numpy_helper.from_array(select_axes2, initia_name)
+            axes1 = onnx.numpy_helper.from_array(select_axes2, initia_name) # creat new initializer
+            # avoid overflows, subtract maximal value
             reducemax_name = old_node.name+"_ReduceMax"
             reducemax_output = old_node.output[0]+'_reducemax'
-            new_scale_node_2 = onnx.helper.make_node(
+            new_scale_node_2 = onnx.helper.make_node( # creat new node
                 op_type="ReduceMax",
                 inputs=old_node.input,
                 outputs=[reducemax_output],
@@ -206,8 +210,8 @@ if args.mode == 'softmax':
                 outputs=old_node.output,
                 name=div_name,
             )
-            graph.node.insert(i, new_scale_node_1)
-            all_initializer.insert(0, axes1)
+            graph.node.insert(i, new_scale_node_1) # insert new node
+            all_initializer.insert(0, axes1) #insert new initializer
             graph.node.insert(i + 1, new_scale_node_2)
             graph.node.insert(i + 2, new_scale_node_3)
             graph.node.insert(i + 3, new_scale_node_4)
@@ -219,18 +223,18 @@ if args.mode=='exp':
     ln_2_value = np.array([np.log(2)], dtype=np.float32)
     base_value = np.array([2], dtype=np.float32)
     for i,j in enumerate(node):
-        if j.name[-3:] == 'Exp':
+        if j.name[-3:] == 'Exp':# The exp node name should end with “Exp”.
             print("######%s######" % i)
             old_node = j
-            onnx_model.graph.node.remove(j)
+            onnx_model.graph.node.remove(j) #remove the old exp node
             print('======update ', old_node.name, '=========')
             ln_2_name = old_node.name+"_ln2_value"
-            value1 = onnx.numpy_helper.from_array(ln_2_value, ln_2_name)
+            value1 = onnx.numpy_helper.from_array(ln_2_value, ln_2_name) # add new initilizer
             base_name = old_node.name + "_base_value"
             value2 = onnx.numpy_helper.from_array(base_value, base_name)
             div_name = old_node.name + '_Div'
             div_output = old_node.output[0]+'_div'
-            new_scale_node_1 = onnx.helper.make_node(
+            new_scale_node_1 = onnx.helper.make_node( # add new node
                 op_type="Div",
                 inputs=[old_node.input[0], ln_2_name],
                 outputs=[div_output],
@@ -243,11 +247,11 @@ if args.mode=='exp':
                 outputs=old_node.output,
                 name=pow_name,
             )
-            all_initializer.insert(0, value1)
+            all_initializer.insert(0, value1) # insert new initilizer
             all_initializer.insert(0, value2)
-            graph.node.insert(i, new_scale_node_1)
+            graph.node.insert(i, new_scale_node_1) # insert new node
             graph.node.insert(i + 1, new_scale_node_2)
 
 
-onnx.save(onnx_model, args.model_path_out)
+onnx.save(onnx_model, args.model_path_out) # save the modified model
 
